5369fb110af6bb01ccb105685ebd7779541e2192
RafatRU42 <rmrafat420@gmail.com>
2023-09-18 09:56:51 +0600

    weframe project started

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span className="inline-block transition-transform group-hover:translate-x-10 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
